ed3d7670149ac18ccb0725c0e01dadd0b5176128
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-09-24 20:17:41 +0700

    created error handler middleware

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8800;
+const { logger } = require('./middleware/logsHandler');
+const { notFoundError, errorHandler } = require('./middleware/errorHandler');
+
+/** CUSTOM LOGGER */
+app.use(logger);
+
+/** ROUTES */
+app.get('/', (req, res) =>{
+    res.status(200).send('<h1>Hello World</h1>')
+});
+
+/** ERROR HANDLER MIDDLEWARE */
+app.all('/{*any}', notFoundError);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
---
+++ middleware/errorHandler.js
@@ -0,0 +1,20 @@
+const CustomError = require('../configs/customError');
+
+const errorHandler = (error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+
+    req.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message
+    })
+};
+
+const notFoundError = (req, res, next) => {
+    const error = new CustomError(`${req.originalUrl} does not exist in the server`, 404);
+};
+
+module.exports = {
+    notFoundError,
+    errorHandler
+}
